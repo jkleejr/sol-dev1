d1924090ef7d014a0c0b9b0f5a5dc3b36f663fa1
jkleejr <jklee@ucsd.edu>
2023-01-05 23:32:23 -0800

    iterating account, program id

--- simplemath/src/sum/src/lib.rs
+++ simplemath/src/sum/src/lib.rs
@@ -1,10 +1,17 @@
+use borsh::{BorshDeserialize, BorshSerialize}; // for borsh dependencies
 use solana_program::{
     account_info::AccountInfo,
     entrypoint,
     entrypoint::ProgramResult, 
     msg,
+    program_error::programError,
     pubkey::Pubkey,
 };
+
+#[derive(BorshDeserialize, BorshDeserialize, Debug)]
+pub struct MathStuffSum {
+    pub sum: u32,
+}
 
 entrypoint!(process_instruction); // establish entrypoint using macro
 
@@ -15,7 +22,19 @@ fn process_instruction( // solana enter program on process_instruction
     _instruction_data: &[u8],    // instruction data, a byte array 
 ) -> ProgramResult {
 
-    msg!("simple smart contract");
+    // iterating accounts is safer than indexing
+    let accounts_iter = &mut accounts.iter();
+
+    // get the account to say hello to
+    let account = next_account_info(accounts_iter)?; 
+
+    // the account must be owned by the program in order to modify its data
+    // big solana rule
+    if account.owner != program_id {
+        msg!("Account does not have the correct program id");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
     Ok(())
 }
 
